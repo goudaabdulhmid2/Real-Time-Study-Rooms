3b8142e051cf21f3e60fa3738846b576608c104c
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2025-09-12 21:38:26 +0300

    Add morgan logging middleware and update package dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,16 @@
-import { Application } from "express";
+import express, { Application } from "express";
+import morgan from 'morgan';
+
+
 import config from "./config/config";
 
 
 const app: Application = config.getApp();
 
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
